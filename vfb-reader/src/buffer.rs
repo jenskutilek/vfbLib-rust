0c335992a8c7c118e82da9ef5ff31b23e00ca1d4
Jens Kutilek <webmail@kutilek.de>
2025-01-10 16:31:37 +0100

    New header format

--- vfb-reader/src/buffer.rs
+++ vfb-reader/src/buffer.rs
@@ -1,6 +1,18 @@
 use std::io::prelude::*;
 use std::io::BufReader;
 
+/// Read the specified number of bytes from a buffer
+pub fn read_bytes<R>(r: &mut BufReader<R>, bytes_to_read: u64) -> Vec<u8>
+where
+    R: std::io::Read,
+{
+    let mut buf = vec![];
+    let mut chunk = r.take(bytes_to_read);
+    let n = chunk.read_to_end(&mut buf).expect("Didn't read enough");
+    assert_eq!(bytes_to_read as usize, n);
+    return buf
+}
+
 /// Read n u8 values from a buffer
 // fn read_n_u8<R>(r: &mut BufReader<R>, n: u8) -> u8
 // where
@@ -10,6 +22,19 @@ use std::io::BufReader;
 //     r.read_exact(&mut buf).expect("ValueError");
 //     return buf[0];
 // }
+
+/// Read the specified number of bytes from a buffer and return them as a string
+pub fn read_str<R>(r: &mut BufReader<R>, bytes_to_read: u64) -> String
+where
+    R: std::io::Read,
+{
+    let buf = read_bytes(r, bytes_to_read);
+    let s= match std::str::from_utf8(&buf) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+    return s.to_string();
+}
 
 /// Read a u8 value from a buffer
 pub fn read_u8<R>(r: &mut BufReader<R>) -> u8
--- vfb-reader/src/header.rs
+++ vfb-reader/src/header.rs
@@ -1,14 +1,15 @@
 use crate::buffer;
 
 use serde::Serialize;
-use std::io::prelude::*;
 use std::io::BufReader;
 
-struct VfbHeaderReserved {
-    data: [u8; 34],
+struct VfbHeaderChunk {
+    // TODO: Two-step decompilation here as in entries.
+    // Now only the raw data is stored, but the internals of the chunk format are unknown.
+    data: Vec<u8>,
 }
 
-impl Serialize for VfbHeaderReserved {
+impl Serialize for VfbHeaderChunk {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::Serializer,
@@ -18,84 +19,75 @@ impl Serialize for VfbHeaderReserved {
 }
 
 #[derive(Serialize)]
+pub struct VfbHeaderCreator {
+    value0: i32,
+    app_version: i32,
+    value1: i32,
+}
+
+#[derive(Serialize)]
 pub struct VfbHeader {
-    header00: u8,
-    filetype: [u8; 5],
-    header01: u16,
-    header02: u16,
-    reserved: VfbHeaderReserved, // TODO: Better store as hexstring like in vfbLib?
-    header03: u16,
-    header04: u16,
-    header05: u16,
-    header06: u16,
-    header07: u16,
-    header08: u16,
-    header09: (u8, i32),
-    header10: (u8, i32),
-    header11: (u8, i32),
-    header12: u8,
-    header13: u16,
-    header14: u16,
+    header0: u8,
+    filetype: String,
+    header1: u16,
+    chunk1: VfbHeaderChunk,
+    creator: VfbHeaderCreator,
+    end0: u8,
+    end1: u8,
+    end2: u16,
 }
 
 pub fn read<R>(r: &mut BufReader<R>) -> VfbHeader
 where
     R: std::io::Read,
 {
-    let header00 = buffer::read_u8(r); // 26
-    let mut filetype = [0u8; 5]; // WLF10
-    r.read_exact(&mut filetype).expect("ValueError");
-    let header01 = buffer::read_u16(r); // 3
-    let header02 = buffer::read_u16(r); // 44
-    let mut res = [0u8; 34]; // 0000000000000000000000000000000000
-    r.read_exact(&mut res).expect("ValueError");
-    let reserved = VfbHeaderReserved { data: res };
-    let header03 = buffer::read_u16(r); // 1
-    let header04 = buffer::read_u16(r); // 0
-    let header05 = buffer::read_u16(r); // 4
-    let header06 = buffer::read_u16(r); // 0
-    let mut header07 = buffer::read_u16(r); // 10
-    let header08: u16;
-    let header09: (u8, i32);
-    let header10: (u8, i32);
-    let header11: (u8, i32);
-    let header12: u8;
-    if header07 == 10 {
-        // FLS5 additions over the FLS2 format
-        header08 = buffer::read_u16(r);
-        header09 = (buffer::read_u8(r), buffer::read_value(r));
-        header10 = (buffer::read_u8(r), buffer::read_value(r));
-        header11 = (buffer::read_u8(r), buffer::read_value(r));
-        header12 = buffer::read_u8(r); // or stop byte?
+    let header0 = buffer::read_u8(r);
+    let filetype = buffer::read_str(r, 5);
+    let header1 = buffer::read_u16(r);
+    let chunk1_size: u64 = buffer::read_u16(r).try_into().unwrap();
+    let res = buffer::read_bytes(r, chunk1_size);
+    let chunk1 = VfbHeaderChunk { data: res };
+    let chunk1_usize: usize = chunk1_size.try_into().unwrap();
+    let last = chunk1.data.as_slice()[chunk1_usize - 1];
+    let last2 = chunk1.data.as_slice()[chunk1_usize - 2];
+    let creator: VfbHeaderCreator;
+    let end0: u8;
+    let end1: u8;
+    if [last2, last] == [10, 0] {
+        // FL4+ additions over FL3
+        let creator_size = buffer::read_u16(r);
+        let _creator_bytes = buffer::read_bytes(r, creator_size.try_into().unwrap());
+        // TODO:
+        // read key, value pairs from creator_bytes until key == 0
+        // We need a buffer::read_value that reads from Vec<u8> instead for that
+        // For now, set the creator header to a constant:
+        creator = VfbHeaderCreator {
+            value0: 0,
+            app_version: 0x05020280i32,
+            value1: 0,
+        };
+        end0 = buffer::read_u8(r);
+        end1 = buffer::read_u8(r);
     } else {
-        // Upgrade the header to FLS5
-        header07 = 10;
-        header08 = 0;
-        header09 = (1u8, 1i32);
-        header10 = (2u8, 0x05020280i32);
-        header11 = (3u8, 0i32);
-        header12 = 0;
+        // Older header format, upgrade it
+        creator = VfbHeaderCreator {
+            value0: 0,
+            app_version: 0x05020280i32,
+            value1: 0,
+        };
+        end0 = 6;
+        end1 = 1;
     }
-    let header13 = buffer::read_u16(r);
-    let header14 = buffer::read_u16(r);
+    let end2 = buffer::read_u16(r);
 
     return VfbHeader {
-        header00,
+        header0,
         filetype,
-        header01,
-        header02,
-        reserved,
-        header03,
-        header04,
-        header05,
-        header06,
-        header07,
-        header08,
-        header09,
-        header10,
-        header11,
-        header12,
-        header13,
-        header14,
+        header1,
+        chunk1,
+        creator,
+        end0,
+        end1,
+        end2,
     };
 }
