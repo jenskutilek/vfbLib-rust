fdefcf6091c50e4c133eac9bc0e91426f3aa77fb
Jens Kutilek <webmail@kutilek.de>
2025-07-07 14:37:26 +0200

    Add some comments

--- vfb-reader/src/entries.rs
+++ vfb-reader/src/entries.rs
@@ -12,14 +12,22 @@ pub enum VfbEntryTypes {
 
 /// Dispatch the decompilation to the appropriate function
 pub fn decompile(entry: &VfbEntry) -> Option<VfbEntryTypes> {
+    // The entry has no data
     if entry.data.is_none() {
         return None;
     }
+
     let bytes = &entry.data.as_ref().unwrap().bytes;
+
+    // The entry has data, but it is empty
     if bytes.len() == 0 {
         return None;
     }
+
+    // FIXME: Do we actually need to combine Cursor + BufReader?
     let mut r = BufReader::new(Cursor::new(bytes));
+
+    // Match the entry key to the appropriate decompile function, return None for unknown keys
     let decompiled = match entry.key.as_str() {
         "Encoding Default" => encoding::decompile(&mut r),
         "Encoding" => encoding::decompile(&mut r),
